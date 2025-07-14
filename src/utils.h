87b5582a2d8e6394ea19b6b8bfa1ab1a82460f25
Sebastian Fischer <sebf.fischer@gmail.com>
2025-07-14 09:02:14 +0200

    perf(layout): just copy for 1d-ish like arrays

--- src/utils.h
+++ src/utils.h
@@ -3,18 +3,25 @@
 
 #include "pjrt.h"
 
-void check_err(const PJRT_Api* api, PJRT_Error* err);
+void check_err(const PJRT_Api *api, PJRT_Error *err);
 
 template <typename src_type, typename dst_type>
-void row_to_col_order(const std::vector<src_type>& src, dst_type* dst,
-                      const std::vector<int64_t>& dims) {
-  if (dims.empty()) {
+void row_to_col_order(const std::vector<src_type> &src, dst_type *dst,
+                      const std::vector<int64_t> &dims) {
+  if (dims.empty() || dims.size() == 1) {
     std::copy(src.begin(), src.end(), dst);
     return;
   }
 
   int64_t total = 1, n = dims.size();
   for (int64_t d : dims) total *= d;
+
+  for (int64_t d : dims) {
+    if (total == d) {
+      std::copy(src.begin(), src.end(), dst);
+      return;
+    }
+  }
 
   std::vector<int64_t> row_strides(n, 1), col_strides(n, 1);
   for (int64_t i = n - 2; i >= 0; --i)
