2eaa8a79b2ee1e515608e3fbc4ceb1d35173c5c1
Sebastian Fischer <sebf.fischer@gmail.com>
2025-07-21 14:38:55 +0200

    vignette

--- R/loaded_executable.R
+++ R/loaded_executable.R
@@ -19,16 +19,25 @@ loaded_executable_execute <- function(
 }
 
 #' @title Execute a PJRT program
-#' @description Execute a PJRT program with the given inputs and execution options.
+#' @description
+#' Execute a PJRT program with the given inputs and execution options.
+#'
+#' **Important:**
+#' Arguments are passed by position and names are ignored.
+#'
 #' @param executable (`PJRTLoadedExecutable`)\cr
 #' A PJRT program.
 #' @param ... (`PJRTBuffer)`\cr
 #'   Inputs to the program.
+#'   Named are ignored and arguments are passed in order.
 #' @param execution_options (`PJRTExecuteOptions`)\cr
 #'   Optional execution options for configuring buffer donation and other settings.
 #' @return `PJRTBuffer` | list of `PJRTBuffers`
 #' @export
 pjrt_execute <- function(executable, ..., execution_options = NULL) {
+  if (!is.null(...names())) {
+    stop("Expected unnamed arguments")
+  }
   loaded_executable_execute(executable, list(...), execution_options)
 }
 
