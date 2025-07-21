df391101c9c0c3943f6c055bbcb070921488c88b
Sebastian Fischer <sebf.fischer@gmail.com>
2025-07-21 11:48:15 +0200

    standardize entrypoint for program creation

--- tests/testthat/test-execution-options.R
+++ tests/testthat/test-execution-options.R
@@ -13,7 +13,7 @@ test_that("execution with options works", {
   skip_if_metal()
 
   path <- system.file("programs/test_hlo.pb", package = "pjrt")
-  program <- program_load(path, format = "hlo")
+  program <- pjrt_program(path = path, format = "hlo")
   executable <- pjrt_compile(program)
 
   data <- 3.0
@@ -54,7 +54,7 @@ test_that("execution with options works", {
 test_that("can donate input", {
   skip_if_metal()
 
-  program <- program_load(
+  program <- pjrt_program(
     system.file("programs/jax-stablehlo-update-param.mlir", package = "pjrt"),
     format = "mlir"
   )
---
+++ tests/testthat/test-loaded_executable.R
@@ -0,0 +1,7 @@
+test_that("arguments must be unnamed", {
+  path <- system.file("programs/test_hlo.pb", package = "pjrt")
+  program <- pjrt_program(path = path, format = "hlo")
+  executable <- pjrt_compile(program)
+  buf <- pjrt_buffer(1)
+  expect_error(pjrt_execute(executable, a = buf, "Expected unnamed arguments"))
+})
