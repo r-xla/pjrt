81b302f4139d295bb69e29d7ff88696bd34847ac
Daniel Falbel <dfalbel@gmail.com>
2025-08-13 10:32:50 -0300

    Remove more skips to test

--- tests/testthat/test-buffer.R
+++ tests/testthat/test-buffer.R
@@ -302,7 +302,6 @@ test_that("element_type returns correct data types", {
 })
 
 test_that("R layout and PJRT layout (2D)", {
-  skip_if_metal()
   path <- system.file("programs/jax-stablehlo-subset-2d.mlir", package = "pjrt")
   program <- pjrt_program(path = path, format = "mlir")
   executable <- pjrt_compile(program)
@@ -322,7 +321,6 @@ test_that("R layout and PJRT layout (2D)", {
 })
 
 test_that("R layout and PJRT layout (3D)", {
-  skip_if_metal()
   path <- system.file("programs/jax-stablehlo-subset-3d.mlir", package = "pjrt")
   program <- pjrt_program(path = path, format = "mlir")
   executable <- pjrt_compile(program)
