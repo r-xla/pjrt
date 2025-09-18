0a9eecaee2c44f7b22974116ef52f6a39179a1f6
Sebastian Fischer <sebf.fischer@gmail.com>
2025-09-18 13:15:39 +0200

    feat(execute): add simplify option

--- tests/testthat/test-loaded_executable.R
+++ tests/testthat/test-loaded_executable.R
@@ -27,3 +27,16 @@ test_that("can return two values", {
   expect_equal(as_array(result[[1]]), 3)
   expect_equal(as_array(result[[2]]), 7)
 })
+
+test_that("single-output returns list when simplify=FALSE", {
+  path <- system.file("programs/jax-stablehlo-no-arg.mlir", package = "pjrt")
+  program <- pjrt_program(path = path, format = "mlir")
+  executable <- pjrt_compile(program)
+  result <- pjrt_execute(executable, simplify = FALSE)
+  expect_list(result, types = "PJRTBuffer", len = 1L)
+  expect_equal(as_array(result[[1]]), 3)
+
+  result <- pjrt_execute(executable, simplify = TRUE)
+  expect_class(result, "PJRTBuffer")
+  expect_equal(as_array(result), 3)
+})
