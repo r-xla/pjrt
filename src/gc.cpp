5b826e7d4cd9e56dc3741a620201119ab4f1072c
Sebastian Fischer <sebf.fischer@gmail.com>
2026-06-01 16:12:33 +0300

    fix

--- src/gc.cpp
+++ src/gc.cpp
@@ -4,8 +4,6 @@
 
 #include <atomic>
 
-#include "deferred_release.h"
-
 namespace rpjrt {
 
 static std::atomic<std::size_t> g_gc_call_count{0};
@@ -14,7 +12,6 @@ void call_r_gc() {
   Rcpp::Function r_gc("gc");
   r_gc(Rcpp::Named("full") = true, Rcpp::Named("verbose") = false);
   R_RunPendingFinalizers();
-  process_pending_releases();
   g_gc_call_count.fetch_add(1, std::memory_order_relaxed);
 }
 
